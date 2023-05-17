e9faf127d08b59c78dfb98d409d203119a71610f
Amir Bahadori <hldvco@gmail.com>
2023-05-17 13:57:40 +0330

    [Start] Scaffolding

---
+++ SrtWordListExtractor/SrtWordListExtractor/Controllers/VersionController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SrtWordListExtractor.Controllers
+{
+    /// <summary>
+    /// Returnt the version
+    /// </summary>
+    [Route("api/[controller]")]
+    [Description("Returnt the version")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return Ok("0.00");
+        }
+    }
+}
