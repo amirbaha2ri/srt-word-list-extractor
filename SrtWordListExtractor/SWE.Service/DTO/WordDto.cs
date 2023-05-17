ca709aef96ef2488304dad07fb015752780b0ba6
Amir Bahadori <hldvco@gmail.com>
2023-05-17 14:34:27 +0330

    [Update] Service Project is Added

---
+++ SrtWordListExtractor/SWE.Service/DTO/WordDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SWE.Service.DTO
+{
+    public class WordDto
+    {
+        public long ID { get; set; }
+        public string Name {  get; set; }
+    }
+}
---
+++ SrtWordListExtractor/SrtWordListExtractor/Controllers/WordController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SWE.Service.DTO;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SrtWordListExtractor.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WordController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public ActionResult<WordDto> Get(int id)
+        {
+            var word = new WordDto()
+            {
+                ID = id,
+                Name = "its working"
+            };
+            return Ok(word);
+        }
+    }
+}
