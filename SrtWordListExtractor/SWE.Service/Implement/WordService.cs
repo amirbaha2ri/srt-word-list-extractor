ac014f1c898fbc22617ceba2a279c36b6ceceb86
Amir Bahadori <hldvco@gmail.com>
2023-05-17 18:01:23 +0330

    [Add] Get words is added

---
+++ SrtWordListExtractor/SWE.Service/Implement/WordService.cs
@@ -0,0 +1,81 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SWE.Service.Implement
+{
+    public class WordService
+    {
+        public WordService()
+        {
+
+        }
+        public List<string> GetWords(string srtContent)
+        {
+
+            string textFile = @"D:\Works\0 Sample Files\srt\topgun.srt";
+            if (File.Exists(textFile))
+            {
+                srtContent = File.ReadAllText(textFile);
+            }
+
+
+
+            var res = new List<string>();
+            string[] souceSrt = Regex.Split(srtContent, @"(?:\r?\n)*\d+\r?\n\d{2}:\d{2}:\d{2},\d{3} --> \d{2}:\d{2}:\d{2},\d{3}\r?\n");
+            foreach (var part in souceSrt)
+            {
+
+                if (HasMoreThanOneWord(part))
+                {
+                    var partWords = BreakTextpart(part);
+                    if (partWords != null)
+                    {
+                        res.AddRange(partWords);
+                    }
+                }
+                else if (part != "")
+                    res.Add(part);
+            }
+            res = res.Distinct().ToList();
+            return res;
+        }
+        private bool HasMoreThanOneWord(string textPart)
+        {
+            if (textPart.Length > 0)
+            {
+                if (textPart.Contains(" "))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private List<string> BreakTextpart(string textPart)
+        {
+            try
+            {
+                var res = new List<string>();
+                textPart = textPart.Replace("[", " ");
+                textPart = textPart.Replace("]", " ");
+                Regex pattern = new Regex("[;{}♪,\t.1234567890]|[\n]|[\r]|[\"]|[\']|[?]|[:]|[\\ ]{2}");
+                pattern.Replace(textPart, " ");
+
+                string[] words = textPart.Split();
+                foreach (var w in words)
+                {
+                    if (w.Length > 1)
+                    {
+                        res.Add(w.ToLower());
+                    }
+                }
+                return res;
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("error",ex);
+            }
+            
+        }
+    }
+}
--- SrtWordListExtractor/SrtWordListExtractor/Controllers/WordController.cs
+++ SrtWordListExtractor/SrtWordListExtractor/Controllers/WordController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SWE.Service.DTO;
+using SWE.Service.Implement;
+using System.Web.Http.Results;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -18,6 +20,22 @@ namespace SrtWordListExtractor.Controllers
                 Name = "its working"
             };
             return Ok(word);
+        }
+        [HttpGet]
+        public ActionResult<List<string>> GetList(string test)
+        {
+            try
+            {
+                var _service = new WordService();
+
+                var res = _service.GetWords(test);
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            
         }
     }
 }
